f27071bd23fe779c3a506901779f2e861a6efeab
Joseph Kinahan <kinahanj25@gmail.com>
2024-02-03 22:15:45 +0000

    using state to rerender components on page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import './App.css';
-import React from 'react';
+import React, { useState} from 'react';
 import GoalList from './components/GoalList/GoalList';
 import NewGoal from './components/NewGoal/NewGoal';
 
 const App = () => {
-  //return React.createElement('h1',{title:'This is....'}, 'How are ya, React here!');
-  //or easier, thru jsx magic:
-
-  const courseGoals = [{id: 'cg1', text: 'Finish the course'},
-  {id: 'cg2', text: 'Learn the main course subjects'},
-  {id: 'cg3', text: 'Help other students in the course Q & A'}];
+  const [courseGoals, setCourseGoals] = useState([
+    {id: 'cg1', text: 'Finish the course'},
+    {id: 'cg2', text: 'Learn the main course subjects'},
+    {id: 'cg3', text: 'Help other students in the course Q & A'}
+]);
   
   const addNewGoalHandler = (newGoal) => {
-    courseGoals.push(newGoal);
-    console.log(courseGoals)
+    setCourseGoals(courseGoals.concat(newGoal));
   }
   
   return (
