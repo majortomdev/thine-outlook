60cdccda228a3aa1373775209d6b53d04a0cceb0
Joseph Kinahan <kinahanj25@gmail.com>
2024-02-03 22:02:25 +0000

    sending props back the other way by passing method call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
-import GoalList from './components/GoalList';
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/NewGoal';
 
 const App = () => {
   //return React.createElement('h1',{title:'This is....'}, 'How are ya, React here!');
@@ -9,9 +10,16 @@ const App = () => {
   const courseGoals = [{id: 'cg1', text: 'Finish the course'},
   {id: 'cg2', text: 'Learn the main course subjects'},
   {id: 'cg3', text: 'Help other students in the course Q & A'}];
+  
+  const addNewGoalHandler = (newGoal) => {
+    courseGoals.push(newGoal);
+    console.log(courseGoals)
+  }
+  
   return (
     <div className='course-goals'>
       <h2>Course goals</h2>
+      <NewGoal onAddGoal={addNewGoalHandler} />
       <GoalList goals={courseGoals}/>
     </div>
   )
