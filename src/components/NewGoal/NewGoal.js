1f2b7441dbbc9aa0de8b51e84ff318937beb41ae
Joseph Kinahan <kinahanj25@gmail.com>
2024-02-05 18:30:27 +0000

    enter text to input dynamically for new goal...v1.0

--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -2,21 +2,27 @@ import React from "react";
 import './NewGoal.css';
 
 const NewGoal = props => {
+    let textEntered = '';
+
     const addGoalHandler = event => {
         event.preventDefault();
 
         const newGoal = {
             id: Math.random().toString(),
-            text: "A whole New Goal"
+            text: textEntered
         };
 
         props.onAddGoal(newGoal);
     };
 
+    const textChangeHandler = event => {
+        textEntered = event.target.value;
+    }
+
 
     return (
         <form className="new-goal" onSubmit={addGoalHandler}>
-            <input type="text" />
+            <input type="text" onChange={textChangeHandler}/>
             <button type="submit">Add Goal</button>
         </form>
     );
