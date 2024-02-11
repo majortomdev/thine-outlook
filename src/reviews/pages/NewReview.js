addb661ae3a0d11a80f2eb6a3599e51979cfdf8c
Joseph Kinahan <kinahanj25@gmail.com>
2024-02-11 13:37:15 +0000

    input validation w/ useReducer refactored 2 handler metrhods into 1

--- src/reviews/pages/NewReview.js
+++ src/reviews/pages/NewReview.js
@@ -1,17 +1,56 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useReducer } from "react";
 
 import Input from "../../shared/components/FormElements/Input";
-import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators';
+import Button from "../../shared/components/FormElements/Button";
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE 
+            } from '../../shared/util/validators';
 import './NewReview.css';
 
+const formReducer = (state, action) => {
+    switch (action.type) {
+        case 'INPUT_CHANGE':
+            let formIsValid= true;
+            for(const inputId in state.inputs){
+                if(inputId === action.inputId){
+                    formIsValid = formIsValid && action.isValid;
+                }else {
+                    formIsValid = formIsValid && state.inputs[inputId].isValid;
+                }
+            }
+            return{
+                ...state,
+                inputs: {
+                    ...state.inputs,
+                    [action.inputId]: {value: action.value, isValid: action.isValid}
+                },
+                isValid: formIsValid
+            }
+        default:
+            return state;
+    }
+};
+
 const NewReview = () => {
+    const[formState, dispatch] = useReducer(formReducer, {
+        inputs: {
+            title: {
+                value: '',
+                isValid: false
+            },
+            review: {
+                value: '',
+                isValid: false
+            }
+        } ,
+        isValid: false
+    });
     
-    const titleInputHandler = useCallback((id, value, isValid) => {
-
-    }, []);
-
-    const reviewInputHandler = useCallback((id, value, isValid) => {
-
+    const inputHandler = useCallback((id, value, isValid) => {
+        dispatch({type: 'INPUT_CHANGE', 
+        value: value, 
+        isValid: isValid, 
+        inputId: id
+    });
     }, []);
 
     return (
@@ -23,7 +62,7 @@ const NewReview = () => {
             label="Title" 
             validators= {[VALIDATOR_REQUIRE()]} 
             errorText= "Please enter a valid title."
-            onInput={titleInputHandler}
+            onInput={inputHandler}
         />
         <Input 
             id="review"
@@ -31,8 +70,9 @@ const NewReview = () => {
             label="Description" 
             validators= {[VALIDATOR_MINLENGTH(5)]} 
             errorText= "Please enter a valid reviewwww.At least 100 characters"
-            onInput={reviewInputHandler}
+            onInput={inputHandler}
         />
+        <Button type="submit" disabled={!formState.isValid}>Add a Review</Button>
     </form>
     );
 };
