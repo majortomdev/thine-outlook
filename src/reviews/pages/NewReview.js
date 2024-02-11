e1c1044645593e85e854fe6a49059741798251ea
Joseph Kinahan <kinahanj25@gmail.com>
2024-02-11 01:58:48 +0000

    useEffect hook used4 state + useCallback to stop inf loop

--- src/reviews/pages/NewReview.js
+++ src/reviews/pages/NewReview.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import Input from "../../shared/components/FormElements/Input";
-import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators';
 import './NewReview.css';
 
 const NewReview = () => {
+    
+    const titleInputHandler = useCallback((id, value, isValid) => {
+
+    }, []);
+
+    const reviewInputHandler = useCallback((id, value, isValid) => {
+
+    }, []);
+
     return (
     <form className="review-form">
         <Input 
+            id="title"
             element="input"
             type="text" 
             label="Title" 
             validators= {[VALIDATOR_REQUIRE()]} 
             errorText= "Please enter a valid title."
+            onInput={titleInputHandler}
+        />
+        <Input 
+            id="review"
+            element="textarea"
+            label="Description" 
+            validators= {[VALIDATOR_MINLENGTH(5)]} 
+            errorText= "Please enter a valid reviewwww.At least 100 characters"
+            onInput={reviewInputHandler}
         />
     </form>
     );
--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 
 import { validate } from '../../util/validators';
 import './Input.css';
@@ -15,7 +15,6 @@ const inputReducer = (state, action ) => {
             return {
                 ...state,
                 isTouched: true,
-                isValid: validate(action.val, action.validators)
             };
         default:
             return state;
@@ -28,6 +27,13 @@ const Input = props => {
         isTouched: false,
         isValid: false
     });
+
+    const { id, onInput } = props;
+    const {value, isValid } = inputState;
+
+    useEffect(() => {
+        onInput(id, value, isValid)
+    }, [id, value, isValid, onInput]);
     
     const changeHandler = event => {
         dispatch({
