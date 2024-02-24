12a219f59d184ecb9de642bd73ad1841e0e61c3d
Joseph Kinahan <kinahanj25@gmail.com>
2024-02-24 16:11:55 +0000

    update method in controller hitting db....

--- sumreview/src/main/java/com/majortomdev/sumreview/controller/UserController.java
+++ sumreview/src/main/java/com/majortomdev/sumreview/controller/UserController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.majortomdev.sumreview.model.User;
@@ -34,8 +37,19 @@ public class UserController {
 		return user;
 	}
 	
-	@PostMapping(path="/user/add", consumes="application/json")
+	@PostMapping("/add")
+	@ResponseStatus(code=HttpStatus.CREATED)
 	public void createUser(@RequestBody User user) {
 		userRepository.save(user);
 	}
+	
+	@PutMapping("/update/{id}")
+	public User updateUser(@PathVariable int id) {
+		User user = userRepository.findById(id).get();
+		user.setUserName(user.getUserName()+"(UPDATED)");
+		userRepository.save(user);
+		return user;
+	}
+	
+	
 }
