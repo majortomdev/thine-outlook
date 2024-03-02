406b6951c7f03dfedfbeec690dbd2bd8b23718f5
Joseph Kinahan <kinahanj25@gmail.com>
2024-03-02 15:19:15 +0000

    new CONTROLLER FOLDER and stripped out req/res logic from ROUTES file to a controller file

---
+++ thine-outlook-backend/controllers/reviews-controller.js
@@ -0,0 +1,41 @@
+const HttpError = require('../models/http-error');
+
+const DUMMY_REVIEWS = [
+    {
+        id: 'r1',
+        title: 'MgGowans Glasnevin',
+        description: 'A Pub/ Restaurant with Irish/ European fare',
+        review: 'Sed congue lobortis mauris, et tincidunt tortor lobortis non. Vestibulum in nibh lectus. Nunc et ligula sit amet nibh auctor commodo. Aenean fringilla ex id eleifend tincidunt. Vestibulum aliquet nibh commodo tortor porta consectetur mattis et enim. Proin arcu velit, dignissim vitae porta sit amet, molestie at erat. Maecenas vel lectus a mi molestie accumsan eu ut tortor. Donec varius massa sit amet lectus posuere, eget eleifend sapien rhoncus. ',
+        user: 'u1'
+    }
+];
+
+
+const getPlaceById = (req, res, next) => {
+    const reviewId = req.params.rid;
+    const review = DUMMY_REVIEWS.find(r => {
+        return r.id === reviewId;
+    });
+
+    if(!review){
+        throw new HttpError('Could not find a review for given id', 404);
+    }
+    res.json({review: review});
+}
+
+const getPlaceByUserId = (req, res, next) => {
+    const userId = req.params.uid;
+    const review = DUMMY_REVIEWS.find(r => {
+        return r.user === userId; 
+    });
+    if(!review){
+        return next(
+            new HttpError('Could not find a review for given id', 404)
+            );
+    }
+
+    res.json({review});
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
--- thine-outlook-backend/routes/reviews-routes.js
+++ thine-outlook-backend/routes/reviews-routes.js
@@ -1,44 +1,14 @@
 const express = require('express');
 
-const HttpError = require('../models/http-error');
+const reviewsControllers = require('../controllers/reviews-controller');
 
 const router = express.Router();
 
-const DUMMY_REVIEWS = [
-    {
-        id: 'r1',
-        title: 'MgGowans Glasnevin',
-        description: 'A Pub/ Restaurant with Irish/ European fare',
-        review: 'Sed congue lobortis mauris, et tincidunt tortor lobortis non. Vestibulum in nibh lectus. Nunc et ligula sit amet nibh auctor commodo. Aenean fringilla ex id eleifend tincidunt. Vestibulum aliquet nibh commodo tortor porta consectetur mattis et enim. Proin arcu velit, dignissim vitae porta sit amet, molestie at erat. Maecenas vel lectus a mi molestie accumsan eu ut tortor. Donec varius massa sit amet lectus posuere, eget eleifend sapien rhoncus. ',
-        user: 'u1'
-    }
-];
 
-router.get('/:rid', (req, res, next) => {
-    const reviewId = req.params.rid;
-    const review = DUMMY_REVIEWS.find(r => {
-        return r.id === reviewId;
-    });
 
-    if(!review){
-        throw new HttpError('Could not find a review for given id', 404);
-    }
-    res.json({review: review});
-});
+router.get('/:rid', );
 
-router.get('/user/:uid', (req, res, next) => {
-    const userId = req.params.uid;
-    const review = DUMMY_REVIEWS.find(r => {
-        return r.user === userId; 
-    });
-    if(!review){
-        return next(
-            new HttpError('Could not find a review for given id', 404)
-            );
-    }
-
-    res.json({review});
-});
+router.get('/user/:uid', );
 
 
 
